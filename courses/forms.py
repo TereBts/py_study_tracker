2a6d18376dc7b1cde396af65511375adea994147
TereBts <teresamadelinebatista@gmail.com>
2025-10-30 19:00:49 +0000

    add date input calendar to create course form

--- courses/forms.py
+++ courses/forms.py
@@ -2,6 +2,9 @@
 from django import forms
 from .models import Course
 
+class DateInput(forms.DateInput):
+    input_type = "date"  # HTML5 date input
+
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
@@ -17,11 +20,17 @@ class CourseForm(forms.ModelForm):
         # use Django’s standard RadioSelect for colour
         widgets = {
             "colour": forms.RadioSelect,
+            "start_date": DateInput(format="%d-m-%Y"),
+            "end_date": DateInput(format="%d-%m-%Y"),
         }
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
+        self.fields["start_date"].input_formats = ["%d-%m-%Y"]
+        self.fields["end_date"].input_formats = ["%d-%m-%Y"]
+        self.fields["start_date"].widget.attrs.update({"placeholder": "DD-MM-YYYY"})
+        self.fields["end_date"].widget.attrs.update({"placeholder": "DD-MM-YYYY"})
 
     def clean_title(self):
         title = self.cleaned_data["title"].strip()
