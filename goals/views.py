04a5657c830364447fcf788ff7d680a03f2c9157
TereBts <teresamadelinebatista@gmail.com>
2025-10-31 14:29:22 +0000

    Create goal view

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
+from django.contrib import messages
+
+from .models import Goal
+from .forms import GoalForm
 
 # Create your views here.
+
+class GoalListView(LoginRequiredMixin, ListView):
+    model = Goal
+    template_name = "goals/goal_list.html"
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+class GoalCreateView(LoginRequiredMixin, CreateView):
+    model = Goal
+    form_class = GoalForm
+    template_name = "goals/goal_form.html"
+    success_url = reverse_lazy("goals:list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "Goal created successfully.")
+        return super().form_valid(form)
+
+class GoalUpdateView(LoginRequiredMixin, UpdateView):
+    model = Goal
+    form_class = GoalForm
+    template_name = "goals/goal_form.html"
+    success_url = reverse_lazy("goals:list")
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, "Goal updated successfully.")
+        return super().form_valid(form)
+
+class GoalDetailView(LoginRequiredMixin, DetailView):
+    model = Goal
+    template_name = "goals/goal_detail.html"
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
