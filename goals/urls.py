a83baae491ed0414836c5060de03df44b215e97e
TereBts <teresamadelinebatista@gmail.com>
2025-10-31 14:39:58 +0000

    add user goals template

--- goals/urls.py
+++ goals/urls.py
@@ -1,6 +1,12 @@
+# apps/goals/urls.py
 from django.urls import path
-from . import views
+from .views import GoalListView, GoalCreateView, GoalUpdateView, GoalDetailView
+
+app_name = "goals"
 
 urlpatterns = [
-    path('', views.GoalListView.as_view(), name='goal_list'),
+    path("", GoalListView.as_view(), name="list"),
+    path("new/", GoalCreateView.as_view(), name="create"),
+    path("<int:pk>/", GoalDetailView.as_view(), name="detail"),
+    path("<int:pk>/edit/", GoalUpdateView.as_view(), name="edit"),
 ]
