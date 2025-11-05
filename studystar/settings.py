0d0083e98411211552e6aa61299edb31689ca302
TereBts <teresamadelinebatista@gmail.com>
2025-11-05 14:46:36 +0000

    Configure static files and middleware for Heroku (add WhiteNoise)

--- studystar/settings.py
+++ studystar/settings.py
@@ -97,6 +97,7 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'studystar.urls'
@@ -167,7 +168,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
