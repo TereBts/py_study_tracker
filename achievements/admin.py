0233cda0b6619466c3eaa697dd9a729ebfedeb72
TereBts <teresamadelinebatista@gmail.com>
2025-11-08 14:24:16 +0000

    add achievements to admin

--- achievements/admin.py
+++ achievements/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Achievement, UserAchievement
 
-# Register your models here.
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ("code", "title", "rule_type", "icon")
+    list_filter = ("rule_type",)
+
+@admin.register(UserAchievement)
+class UserAchievementAdmin(admin.ModelAdmin):
+    list_display = ("user", "achievement", "awarded_at")
+    list_filter = ("achievement",)
