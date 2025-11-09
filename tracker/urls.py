65ed7a2f915273a5dce4364064afdf7079948950
TereBts <teresamadelinebatista@gmail.com>
2025-11-09 19:48:40 +0000

    style logout page

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "tracker"
 
 urlpatterns = [
-    path("home/", views.home, name="home"),                 # public
+    path("", views.home, name="home"),                  # now '/' is home
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
-    path("dashboard/", views.dashboard, name="dashboard"), # private
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
